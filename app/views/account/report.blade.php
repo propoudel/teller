381223d887d56fe6f1b6216c3d1682d7a590a24f
propoudel <developer.sagarpoudel@gmail.com>
2015-05-18 23:20:02 +0400

    error fixed

--- app/views/account/report.blade.php
+++ app/views/account/report.blade.php
@@ -1,12 +1,23 @@
 @extends("layout")
 @section("content")
+@extends("layout")
+@section("content")
 
   <div class="panel panel-default">
           <div class="panel-heading">Report</div>
           <div class="panel-body">
               <div class="col-sm-12">
-                  <form action="<?php echo URL::to('/report/create'); ?>" method="POST">
+                  <form action="<?php echo URL::to('/account/store'); ?>" method="POST" name="addTransfer" id="addTransfer">
+                      <div class="row">
 
+                          <div class="col-sm-12">
+
+                              <div class="form-group">
+                                  <label></label>
+                              </div>
+                          </div>
+
+                      </div>
                       <div class="row">
 
                       <div class="col-sm-3">
@@ -108,65 +119,12 @@
                           </div>
                       </div>
                   </form>
-=======
-          <div class="panel-heading">Currency</div>
-          <div class="panel-body">
-              <div class="col-sm-12">
-                <div class="form-group">
-                  <a href="/currency/create" class="btn btn-primary">Add New</a>
-                 </div>
-
-                 @if($errors->has())
-                     @foreach($errors->all() as $message)
-                      <div class="alert alert-info">
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        {{ $message }}
-                      </div>
-                     @endforeach
-                  @endif
-
-                 @if(Session::has('message'))
-                     <div class="alert alert-info">
-                     <button type="button" class="close" data-dismiss="alert">&times;</button>
-                     {{ Session::get('message') }}
-                   </div>
-                 @endif
-
-
-
-                <table id="currency" class="display table" cellspacing="0" width="100%">
-                				<thead>
-                					<tr>
-                						<th>SN.</th>
-                						<th>Currency Code</th>
-                						<th>Rate</th>
-                						<th style="width: 120px;">Action</th>
-
-                					</tr>
-                				</thead>
-
-                				<tbody>
-                				    <?php $i=1 ?>
-                                    @foreach($data as $list)
-                					<tr>
-                						<td>{{ $i }}</td>
-                						<td>{{ $list->currency_code }}</td>
-                						<td>{{ $list->currency_rate }}</td>
-                						<td>
-                                            <a class="btn btn-small btn-info" href="{{ URL::to('currency/' . $list->id . '/edit') }}">Edit</a>
-                                             <a class="btn btn-small btn-success" href="{{ URL::to('currency/' . $list->id. '/delete') }}" onclick="if(!confirm('Are you sure?')){ return false;}">Delete</a>
-                						</td>
-
-                					</tr>
-                					<?php $i++ ?>
-                					@endforeach
-
-                				</tbody>
-                			</table>
 
               </div>
 
           </div>
       </div>
+
+@stop
 
 @stop
