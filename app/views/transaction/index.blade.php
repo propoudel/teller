f2cf244c4402d2ba160de7db999aafcd08366d56
Prakash <prakashluitel88@gmail.com>
2015-06-15 22:17:58 +0400

    Teller added reference if

--- app/views/transaction/index.blade.php
+++ app/views/transaction/index.blade.php
@@ -8,7 +8,7 @@
                 <table id="party" class="display table" cellspacing="0" width="100%">
                     <thead>
                     <tr>
-                        <th>SN.</th>
+                        <th>Reference Id</th>
                         <th>Debtor</th>
                         <th>Creditor</th>
                         <th>Created At</th>
@@ -20,7 +20,7 @@
                     <?php $i=1 ?>
                     @foreach($data['trans'] as $list)
                         <tr>
-                            <td>{{ $i }}</td>
+                            <td>{{ $list->reference_id }}</td>
                             <td>{{ $list->debtor }}</td>
                             <td>{{ $list->creditor }}</td>
                             <td>{{ $list->created_at }}</td>
@@ -28,7 +28,6 @@
                                 <a class="btn btn-small btn-info" href="{{ URL::to('transaction/' . $list->id . '/edit') }}">Edit</a>
                             <!--<a class="btn btn-small btn-success" href="{{ URL::to('account/' . $list->id. '/delete') }}" onclick="if(!confirm('Are you sure?')){ return false;}">Delete</a>-->
                             </td>
-
                         </tr>
                         <?php $i++ ?>
                     @endforeach
